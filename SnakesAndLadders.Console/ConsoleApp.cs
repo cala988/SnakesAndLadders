6b6e2d4635aa1ec488b4b06166278a814a6c8a6f
clafuente <clafuente@atsistemas.com>
2022-10-17 10:44:08 +0200

    Add logic to ConsoleApp

--- SnakesAndLadders.Console/ConsoleApp.cs
+++ SnakesAndLadders.Console/ConsoleApp.cs
@@ -1,25 +1,79 @@
 ﻿using Microsoft.Extensions.Hosting;
+using SnakesAndLadders.Application.Entitites;
+using SnakesAndLadders.Application.Interfaces;
+
 namespace SnakesAndLadders
 {
     public class ConsoleApp : IHostedService
     {
-        public ConsoleApp()
+        private const string RollCommand = "roll";
+        private const string StopCommand = "stop";
+        private readonly IBoardGameService _boardGameService;
+
+        public ConsoleApp(IBoardGameService boardGameService)
         {
+            _boardGameService = boardGameService;
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            //TODO.. call to logic
-
+            InitGame();
             return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            //TODO.. call to logic in case of error
-
-            //_logger.LogInformation("Aplicación parada.");
             return Task.CompletedTask;
+        }
+
+        public void InitGame()
+        {
+            Console.WriteLine("Hello this is the game SnakesAndLadders.");
+            Console.WriteLine("Write stop to exit the game.");
+            Console.WriteLine("Set number of players:");
+            var command = Console.ReadLine();
+            bool isNumber = int.TryParse(command, out int numberOfPlayers);
+            while (!isNumber)
+            {
+                Console.WriteLine("Number of players must be a number, please reenter the number of players");
+                command = Console.ReadLine();
+                isNumber = int.TryParse(command, out numberOfPlayers);
+            }
+            _boardGameService.Start(numberOfPlayers);
+            bool playerWonGame = false;
+            while (command != StopCommand && !playerWonGame)
+            {
+                for (int actualPlayer = 1; actualPlayer <= numberOfPlayers; actualPlayer++)
+                {
+                    Console.WriteLine($"Player {actualPlayer}, please write roll to throw the die.");
+                    command = Console.ReadLine();
+                    
+                    if (command.Equals(StopCommand))
+                    {
+                        Console.WriteLine("The game is over.");
+                        Console.ReadKey();
+                        break;
+                    }
+
+                    if (command.Equals(RollCommand))
+                    {
+                        var player = _boardGameService.GetPlayer(actualPlayer);
+                        int initialSquareOfTurn = _boardGameService.GetTokenPositionOfPlayer(player);
+                        int number = _boardGameService.RollDie();
+                        _boardGameService.MovePlayerTokenPosition(player, number);
+                        player = _boardGameService.GetPlayer(actualPlayer);
+                        int finalSquareOfTurn = _boardGameService.GetTokenPositionOfPlayer(player);
+                        Console.WriteLine($"You have rolled an {number}, you have moved from square {initialSquareOfTurn} to {finalSquareOfTurn}.");
+                        playerWonGame = _boardGameService.CheckIfThePlayerWonTheGame(player);
+                        if (playerWonGame)
+                        {
+                            Console.WriteLine($"Congratulations! player {number} won the game! The game is over.");
+                            Console.ReadKey();
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- SnakesAndLadders.Console/Program.cs
+++ SnakesAndLadders.Console/Program.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SnakesAndLadders;
+using SnakesAndLadders.Application.Interfaces;
+using SnakesAndLadders.Application.Services;
 
 IHost host = Host
     .CreateDefaultBuilder(args)
 
     .ConfigureServices(services =>
-    {   
+    {
+        services.AddTransient<IDieService, DieService>();
+        services.AddTransient<IBoardGameService, BoardGameService>();
+
         services.AddSingleton<IHostedService, ConsoleApp>();
     })
     .Build();
